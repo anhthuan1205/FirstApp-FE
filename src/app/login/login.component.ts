1e76278936ee11b33978cf75fc319961e87d2d17
anhthuan95 <thuan.hust.95@gmail.com>
2020-01-10 10:20:33 +0700

    add login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,55 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../auth/auth.service';
+import {TokenStorageService} from '../auth/token-storage.service';
+import {Router} from '@angular/router';
+import {ProfileService} from '../service/profile.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  isLoggedIn = false;
+  message: string;
+  id: number;
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private tokenStorage: TokenStorageService,
+    private profileService: ProfileService,
+    private  router: Router) {
+  }
+  ngOnInit() {
+    this.profileService.getOneAccToken().subscribe(next => {
+      this.isLoggedIn = true;
+      console.log('lay duoc profile');
+    }, error => this.isLoggedIn = false ) ;
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+    this.tokenStorage.saveToken(this.tokenStorage.getTokenCookies());
+  }
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const {value} = this.loginForm;
+      console.log(value);
+      this.authService.attemptAuth(value)
+        .subscribe(next => {
+          console.log(next);
+          this.tokenStorage.saveToken(next.accessToken);
+          this.isLoggedIn = true;
+          this.message = 'Thành công';
+          this.profileService.getOneAccToken().subscribe(next2 => {
+            console.log('vao day chua');
+            console.log(next2);
+            this.tokenStorage.saveEmail(next2.email);
+            console.log('lay duoc profile');
+          }, error => console.log('lay duoc profile') ) ;
+        }, error => this.message = 'Lỗi đăng nhập, sai email hoặc mật khẩu, vui lòng nhập lại');
+    }
+  }
+}
---
+++ src/app/service/profile.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {IProfile} from '../interface/i-profile';
+import {JwtResponse} from '../interface/JwtResponse';
+import {LinkAPIService} from './link-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+  constructor(private http: HttpClient,
+              private url: LinkAPIService) {
+  }
+
+  getOneAcc(id: number): Observable<IProfile> {
+    return this.http.get<IProfile>(`${this.url.link}/user/${id}`);
+  }
+
+  updateAcc(user: Partial<IProfile>): Observable<JwtResponse> {
+    const r = confirm('Ban chac chan muon cap nhat?\n Chon OK hoac Cancel!');
+    if (r) {
+      return this.http.put<JwtResponse>(`${this.url.link}/profile/edit`, user);
+    }
+  }
+
+    getOneAccToken(): Observable<IProfile> {
+    return this.http.get<IProfile>(`${this.url.link}/profile`);
+  }
+}
