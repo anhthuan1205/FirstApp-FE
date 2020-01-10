70d2f1fd459e578b927a93369f953b3e9191a49f
anhthuan95 <thuan.hust.95@gmail.com>
2020-01-10 15:52:35 +0700

    show data success

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IProfile } from '../interface/i-profile';
+import { ProfileService} from '../service/profile.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  profile: IProfile;
+  data: FormGroup;
 
-  constructor() { }
+  constructor(private profileService: ProfileService,
+              private fb: FormBuilder) {}
 
   ngOnInit() {
+    // this.data = this.fb.group({
+    //   id: '',
+    //   name: '',
+    //   email: ''
+    // });
+    this.profileService.getOneAccToken().subscribe( next => {
+      this.profile = next;
+      console.log(next);
+      // this.data.patchValue(this.profile);
+      }
+    );
   }
 
 }
